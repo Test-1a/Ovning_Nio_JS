3ee49ab6eb1ee27e50d527abb7a5c67eae6d2e84
Mikael <mikael@sehlin.net>
2019-12-02 03:10:43 +0100

    Add project files.

---
+++ Ovning_Nio_JS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ovning_Nio_JS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ovning_Nio_JS/script1.js
@@ -0,0 +1,19 @@
+﻿//alert("Hi");
+
+document.getElementById("fun").addEventListener("click", function () { BishBosh(); })
+
+function BishBosh() {
+    //alert("Button clicked!")
+    var i;
+    for (i = 1; i <= 100; i++)
+    {
+        if ((i % 4 === 0) && (i % 3 === 0))
+            console.log("Bish-Bosh");
+        else if (i % 4 === 0)
+            console.log("Bosh");
+        else if (i % 3 === 0)
+            console.log("Bish");
+        else
+            console.log(i);
+    }
+}
